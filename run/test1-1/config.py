Fix bug in rhs scaling
Working now

run/test1-1/config.py
@@ -4,17 +4,19 @@ sys.path.append("../../source/")
 from euler_solver import *
 
 s = EulerSolver()
-m = Grid(0,1,50)
+m = Grid(0,1,67)
 s.set_mesh(m)
-s.distribute_dofs(3)
+s.distribute_dofs(2)
 def ic_func_prim(x_dof, x_cell):
     if x_cell<0.3: return np.array([1,0.75,1])
     else: return np.array([0.125,0,0.1])
 s.set_states(ic_func_prim)
 s.set_surface_flux()
 s.set_volume_flux()
-def bc_free(x,t,cons):
-    return cons
-s.set_bc_funcs(bc_free, bc_free)
-s.set_time_controls(0,0.2,1)
+def bc_left(t,cons):
+    return Euler.prim_to_cons(np.array([1,0.75,1]))
+def bc_right(t,cons):
+    return Euler.prim_to_cons(np.array([0.125,0,0.1]))
+s.set_bc_funcs(bc_left, bc_right)
+s.set_time_controls(0,0.2,10,CFL=0.1)
 s.run()

source/euler_solver.py
@@ -73,7 +73,7 @@ class EulerSolver:
     
     def set_bc_funcs(self, left_func, right_func):
         # sets the BC functions
-        # the functions should take location, time and inner state as input,
+        # the functions should take time and inner state as input,
         # and return the ghost state
         self.bc_func_left = left_func
         self.bc_func_right = right_func
@@ -153,22 +153,21 @@ class EulerSolver:
         surface_fluxes = np.zeros(self.mesh.n_cells+1, dtype=object)
         cons = self.states.entries[0]
         surface_fluxes[0] = self.surface_flux(
-            self.bc_func_left(self.mesh.x_faces[0], self.time, cons),
+            self.bc_func_left(self.time, cons),
             cons,
             self.alpha.entries[0]
         )
         cons = self.states.entries[-1]
         surface_fluxes[-1] = self.surface_flux(
             cons,
-            self.bc_func_right(self.mesh.x_faces[-1], self.time, cons),
+            self.bc_func_right(self.time, cons),
             self.alpha.entries[-1]
         )
         for i_face in range(1, self.mesh.n_cells):
             i_dof_left = (i_face-1)*(N+1) + N
-            i_dof_right = i_face*(N+1)
             surface_fluxes[i_face] = self.surface_flux(
                 self.states.entries[i_dof_left],
-                self.states.entries[i_dof_right],
+                self.states.entries[i_dof_left+1],
                 0.5*(self.alpha.entries[i_face-1] + self.alpha.entries[i_face])
             )
         
@@ -224,6 +223,7 @@ class EulerSolver:
                 rhs[cell_dofs[N]] += (
                     cell_alpha*surface_fluxes[i_cell+1]/self.dof_handler.quad.q_weights[N]
                 )
+        rhs *= -self.mesh.Jinv
         return rhs
     
     def calc_time_step(self):

source/grid.py
@@ -11,4 +11,4 @@ class Grid:
         self.dx = self.x_faces[1] - self.x_faces[0] # cell width (uniform)
         self.x_cells = np.linspace(x_left+0.5*self.dx, x_left+length-0.5*self.dx, n_cells)
         self.J = 0.5*self.dx # Jacobian of mapping from any cell to reference space [-1,1]
-        self.Jinv = 1/self.J
+        self.Jinv = 1.0/self.J