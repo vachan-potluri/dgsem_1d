47e4909384797b98f691ea75b7a3e4d13c1482ff
Vachan Potluri <vachanpotluri1997@gmail.com>
2023-03-24 09:54:09 +0530

    Write function to set BC and calculate alpha

--- source/euler_solver.py
+++ source/euler_solver.py
@@ -12,10 +12,13 @@ class EulerSolver:
     
     def __init__(self):
         # does nothing, just initialises the instance variables to None
-        self.mesh = None
-        self.dof_handler = None
-        self.states = None
         self.alpha = None
+        self.bc_func_left = None
+        self.bc_func_right = None
+        self.cbm = None
+        self.dof_handler = None
+        self.mesh = None
+        self.states = None
     
     def set_mesh(self, mesh):
         # sets the mesh, and variables that are defined on the mesh
@@ -36,6 +39,8 @@ class EulerSolver:
         print(f"\t# dofs: {self.dof_handler.n_dofs}")
         print("Initialising state vector")
         self.states = DoFVector(self.dof_handler, Euler.n_vars)
+        print("Setting change of basis matrix")
+        self.cbm = ChangeBasisMatrix(N)
     
     def set_states(self, func, prim=True):
         # sets the states based on the function provided
@@ -57,3 +62,49 @@ class EulerSolver:
                         self.dof_handler.x_dofs[i_dof],
                         self.mesh.x_cells[i_cell]
                     )
+    
+    def set_bc_funcs(self, left_func, right_func):
+        # sets the BC functions
+        # the functions should take location, time and inner state as input,
+        # and return the ghost state
+        self.bc_func_left = left_func
+        self.bc_func_right = right_func
+    
+    def calc_blender(self):
+        # calculates the blender values
+        # currently uses "pressure times density"
+        # currently uses Hennemenn's algorithm (as opposed to Persson's)
+        Np1 = self.dof_handler.N+1
+        pxrho_nodal_values = np.zeros(Np1)
+        threshold = 0.5*10**(-1.8*Np1**0.25)
+
+        # first loop: calculate and clip
+        for i_cell in range(self.mesh.n_cells):
+            for i_dof_local in range(Np1):
+                prim = Euler.cons_to_prim(self.states.entries[i_cell*(Np1)+i_dof_local])
+                pxrho_nodal_values[i_dof_local] = prim[0]*prim[2]
+            pxrho_modes = self.cbm.get_modes(pxrho_nodal_values)
+            energies = pxrho_modes*pxrho_modes
+            trouble = max(energies[-1]/np.sum(energies), energies[-2]/np.sum(energies[:-1]))
+            alpha = 1/(1+np.exp(-9.21024*(trouble/threshold-1)))
+            if alpha < 1e-3:
+                self.alpha.entries[i_cell] = 0.0
+            elif alpha > 0.5:
+                self.alpha.entries[i_cell] = 0.5
+            else:
+                self.alpha.entries[i_cell] = alpha
+        
+        # second loop: diffuse
+        alpha_old = self.alpha.entries.copy()
+        for i_cell in range(self.mesh.n_cells):
+            if i_cell == 0:
+                i_nbs = [i_cell+1]
+            elif i_cell == self.mesh.n_cells-1:
+                i_nbs = [i_cell-1]
+            else:
+                i_nbs = [i_cell-1, i_cell+1]
+            self.alpha.entries[i_cell] = max(
+                alpha_old[i_cell],
+                0.5*np.max(alpha_old[i_nbs])
+            )
+            
--- test/_euler_solver.py
+++ test/_euler_solver.py
@@ -6,8 +6,12 @@ m = Grid(-1,2,10)
 s.set_mesh(m)
 s.distribute_dofs(3)
 def ic_func_prim(x_dof, x_cell):
-    if x_cell<0: return np.array([1,0.75,1])
+    if x_dof<0.1: return np.array([1,0.75,1])
     else: return np.array([0.125,0,0.1])
 s.set_states(ic_func_prim)
 for i in range(s.dof_handler.n_dofs):
     print(f"x = {s.dof_handler.x_dofs[i]:1.4f}, state = {s.states.entries[i]}")
+
+s.calc_blender()
+print("Blender values:")
+print(s.alpha.entries)
